081ecded4a2c0ce7b4c70356dcb87bb5f9a17a29
Girishbabu <59991491+GirishbabuGuthula@users.noreply.github.com>
2024-04-05 14:47:52 +0530

    Add files via upload

---
+++ Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Girish Babu on 9/2/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
